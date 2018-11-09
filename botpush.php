b62ce105645e92cff9924ddc16de77d24232a17f
losaza106 <40599246+losaza106@users.noreply.github.com>
2018-11-09 14:49:23 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'MBNoWbQjzLe1EInL2DTdqihV8nLCAdAseW4fI4ZaGB5HAQkwfI8TWFKH2TmGDcV3ba46f3eQ8tMaHU0O72SupS9Vhv9XzMBav+QPedE1aGjDi4vLxFgpYI0pXfLn83tJ/TzdoILLpqSUI6nmQnsIlQdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U0f23ff85a4c279297122f8e601e36bad';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
