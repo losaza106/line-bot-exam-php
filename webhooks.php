dfb79a11e5dd4ed8699efbecbbb6badd14713cb0
losaza106 <40599246+losaza106@users.noreply.github.com>
2018-11-09 14:18:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'MBNoWbQjzLe1EInL2DTdqihV8nLCAdAseW4fI4ZaGB5HAQkwfI8TWFKH2TmGDcV3ba46f3eQ8tMaHU0O72SupS9Vhv9XzMBav+QPedE1aGjDi4vLxFgpYI0pXfLn83tJ/TzdoILLpqSUI6nmQnsIlQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
