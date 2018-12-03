a8730aa623d75fb6ec75aceba53a4a16eb6713dd
losaza106 <40599246+losaza106@users.noreply.github.com>
2018-12-03 21:46:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." ".$rand_number
+				'text' => "รหัสยืนยันของคุณคือ : ".$rand_number
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
